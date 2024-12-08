bb2fe1f530ceccb79653cefdeb362d4d3fea8553
KarimAbdr <abdrakhamnov.karim67@gmail.com>
2024-12-08 23:13:24 +0100

    Initial commit

--- src/main/java/connection/DB.java
+++ src/main/java/connection/DB.java
@@ -11,16 +11,8 @@ public class DB {
         String password = "123";
         Connection connection=null;
         try {
-            // Load the PostgreSQL JDBC driver
             Class.forName("org.postgresql.Driver");
-
-            // Establish a connection to the database
             connection = DriverManager.getConnection(url, user, password);
-
-            // Now you can use the 'connection' object to interact with the database
-
-            // Don't forget to close the connection when you're done
-
         } catch (ClassNotFoundException e) {
             System.err.println("PostgreSQL JDBC driver not found");
         } catch (SQLException e) {
